7404cc0626eafe9d91536ee237ec66ada6ffa09e
debbyblessing <debbyblessing1234@gmail.com>
2022-02-28 11:21:24 +0100

    adding js properties

--- index.js
+++ index.js
@@ -1,10 +1,68 @@
-const navToggle = document.getElementById('navToggle');
-const nav = docoment.getElementById('nav');
-const navIcon = document.getElemenByAll('.navIcon');
+//
+//const navtoggle = document.getElementById('navToggle');
+//const nav = document.getElementById('nav');
+//let navIcon = document.getElemenByAll('.navIcon');
 
-navToggle.addEventListener('click', () => {
-  nav.classList.toggle('open');
-  navIcon.forEach((icon) => {
-    icon.classList.toggle('hidden');
-  });
+//navtoggle.addEventListener('click', function () {
+
+//nav.classList.toggle('what is ur name') ;
+// navIcon.forEach((icon) => {
+//icon.classList.toggle('hidden');
+//console.log('navtoggle')
+//});
+
+//function openNav() {
+//var  openNav= document.getElementById('links');
+//if ( openNav.style.display === 'block') {
+//closeNav.style.display = 'none';
+//} else {
+////openNav.style.display = 'block';
+//
+//var closeNav= document.getElemenByAll('.navIcon');
+//if (closeNav.style.display === 'block') {
+//closeNav.style.display = 'none';
+//} else {
+//closeNav.style.display = 'block';
+
+//*
+
+/*const openNav= document.querySelector('.openNav');
+var links= document.querySelector('#links');
+var closeNav= document.querySelector('.closeNav');
+
+var myFunction ='closed'
+
+openNav.addEventListener('click', function() {
+
+  if (myFunction === 'closed') {
+    links.style.display = 'block'
+    myFunction = 'open'
+  } else {
+    links.style.display = 'none'
+    myFunction = 'closed'
+  }
 });
+
+closeNav.addEventListener('click',function(){
+  links.style.display = 'none';
+  myFunction = 'closed';
+});
+
+
+window.addEventListener('mouseup', function(e) {
+  var x = document.querySelector('#links');
+  if (event.target != document.querySelector(".icon")) {
+     x.style.display = "none";
+  }
+});
+*/
+
+const menu = document.querySelector('mobile-menu');
+const menulist = document.querySelector('nav-menu');
+
+menu.addEventListener('click', function () {
+  menu.classList.toggle('.is-active');
+  menuLists.classList.toggle('active');
+});
+
+console.log();
