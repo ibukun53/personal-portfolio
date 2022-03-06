09d3c30f0983492233871bc40fdaa3b47c6cdf7d
debbyblessing <debbyblessing1234@gmail.com>
2022-03-06 13:40:40 +0100

    addding js

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 const menu = document.querySelector('#mobile-menu');
 const menuLinks = document.querySelector('.nav-menu');
 // Display mobile menu
-const mobieMenu = () => {
+const mobileMenu = () => {
   menu.classList.toggle('is-active');
   menuLinks.classList.toggle('active');
 };
-menu.addEventListener('click', mobieMenu);
-function myFunction() {
-  document.getElementById('demo').innerHTML = 'Hello World';
-}
+menu.addEventListener('click', mobileMenu);
+function myFunction() {}
+// Display nav menu
+const navMenu = () => {
+  menu.classList.toggle('is-active');
+  menuLinks.classList.toggle('active');
+};
+menuLinks.addEventListener('click',navMenu);
+function myFunction() {}
