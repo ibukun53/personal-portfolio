e070ad3a7bed47efa45dc26649650fcdb4c8d90b
debbyblessing <debbyblessing1234@gmail.com>
2022-03-06 21:28:56 +0100

    fixing error

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 const menu = document.querySelector('#mobile-menu');
 const menuLinks = document.querySelector('.nav-menu');
 // Display mobile menu
-const mobileMenu = () => {
+const mobileMenu   = () => {
   menu.classList.toggle('is-active');
   menuLinks.classList.toggle('active');
 };
 menu.addEventListener('click', mobileMenu);
-// Display nav menu
-const navMenu = () => {};
-menuLinks.addEventListener('click',navMenu);
+menuLinks.addEventListener('click', mobileMenu);
