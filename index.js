ae2c8182c29240b20a507a7c1476dd42628d455b
Blessing Olusanya <75586791+ibukun53@users.noreply.github.com>
2022-03-16 23:16:38 +0100

    Add-Mobile-Menu (#6)

---
+++ index.js
@@ -0,0 +1,9 @@
+const menu = document.querySelector('.navbar-container');
+const menuLinks = document.querySelector('.nav-link');
+// Display mobile menu
+const navbarContainer = () => {
+  menu.classList.toggle('is-active');
+  menuLinks.classList.toggle('active');
+};
+menu.addEventListener('click', navbarContainer);
+menuLinks.addEventListener('click', navbarContainer);
