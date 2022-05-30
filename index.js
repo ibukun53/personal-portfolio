b2bf95a7c85439355a68e22bdc069f0810ab07ac
debbyblessing <debbyblessing1234@gmail.com>
2022-05-30 11:53:51 +0100

    fixed

--- index.js
+++ index.js
@@ -7,3 +7,53 @@ const navbarContainer = () => {
 };
 menu.addEventListener('click', navbarContainer);
 menuLinks.addEventListener('click', navbarContainer);
+
+ 
+const cards = [
+  {
+    image: './image/language.svg',
+    title: 'Language',
+    languages: ['Javascript', 'Ruby', 'Html', 'Css'],
+  },
+  {
+    image: './image/framework.svg',
+    title: 'Framework',
+    languages: ['Bootstrap', 'Capybara', 'Selenium', 'Ruby', 'RSpec'],
+  },
+  {
+    image: './image/skill.svg',
+    title: 'Skills',
+    languages: ['Codekit', 'Github', 'Codepen', 'Gitlab', 'Terminal'],
+  },
+];
+
+function attriCards(data) {
+  this.image = data.image;
+  this.title = data.title;
+  this.languages = data.languages;
+  this.createElement = function createElement() {
+    const generalLiElements = (textContents) =>
+      textContents
+        .map((textContent) => `<li class="attribute-tags contrast">${textContent}</li>`)
+        .join('');
+    const languageLiElements = generalLiElements(this.languages);
+    const div = document.createElement('div');
+    div.innerHTML = ` <div class="flex-container margin-left-right-auto">
+    <div class="flex-box">
+    <p class="attribute-ellip">
+      <img class="attribute-img" src="${this.image}" alt="skill" />
+    </p>
+    <h3 class="attribute-title">${this.title}</h3>
+    <ul class="attri-content">${languageLiElements}</ul>
+  </div> 
+  </div>`;
+    return div;
+  };
+}
+const div = document.querySelector('.flex-container');
+function attachToDom(detail) {
+  const portfolioCard = new attriCards(detail);
+  const portfolio = portfolioCard.createElement();
+  div.appendChild(portfolio);
+}
+cards.forEach(attachToDom);
