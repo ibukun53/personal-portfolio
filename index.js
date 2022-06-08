5c2235e3f7b7ab71b047610516d9592101e3cad3
debbyblessing <debbyblessing1234@gmail.com>
2022-06-08 16:36:13 +0100

    implementing new prop obj

--- index.js
+++ index.js
@@ -16,104 +16,109 @@ const aboutMecards = [
   },
 ];
 
-function createAboutMecardElement(data) {
+function createAboutMecardElements(data) {
   this.image = data.image;
   this.title = data.title;
   this.items = data.items;
   this.createElement = function createElement() {
     const generalLiElements = (textContents) =>
-      textContents
-        .map((textContent) => `<li class="attribute-tags contrast">${textContent}</li>`)
-        .join('');
+      textContents.map((textContent) => `<li class="attribute-tags contrast">${textContent}</li>`).join('');
     const languageLiElements = generalLiElements(this.items);
-    const liElement = document.createElement('li');
-    liElement.classList.add('flex-box');
-    liElement.innerHTML = `<article><p class="attribute-ellip">
+    const liElements = document.createElement('li');
+    liElements.classList.add('flex-box');
+    liElements.innerHTML = `<article><p class="attribute-ellip">
       <img class="attribute-img" src="${this.image}" alt="skill" /> 
     </p>
     <h3 class="attribute-title">${this.title}</h3>
     <ul class="attri-content">${languageLiElements}</ul></article>`;
-    return liElement;
+    return liElements;
   };
 }
 
 const aboutMecardWrapperElement = document.querySelector('.flex-container');
-
 function attachAboutMeDom(detail) {
-  const portfolioCard = new createAboutMecardElement(detail);
+  const portfolioCard = new createAboutMecardElements(detail);
   const portfolio = portfolioCard.createElement();
   aboutMecardWrapperElement.appendChild(portfolio);
 }
-
 aboutMecards.forEach(attachAboutMeDom);
+
 
 const recentWorkCards = [
   {
     image: './image/Screenshot.png',
     title: 'Multi-Post Stories Gain+Glory',
     items: ['Javascript', 'Ruby on rails', 'Html', 'Css'],
+    id: '4aa',
   },
   {
     image: './image/Screenshot.png',
     title: 'Multi-Post Stories Gain+Glory',
     items: ['Javascript', 'Ruby on rails', 'Html', 'Css'],
+    id:  '2cc' ,
   },
   {
     image: './image/Screenshot.png',
     title: 'Multi-Post Stories Gain+Glory',
     items: ['Javascript', 'Ruby on rails', 'Html', 'Css'],
+    id: '4bb',
   },
   {
     image: './image/Screenshot.png',
     title: 'Multi-Post Stories Gain+Glory',
     items: ['Javascript', 'Ruby on rails', 'Html', 'Css'],
+    id: '8hh',
   },
   {
     image: './image/Screenshot.png',
     title: 'Multi-Post Stories Gain+Glory',
     items: ['Javascript', 'Ruby on rails', 'Html', 'Css'],
+    id:  '7ff',
   },
   {
     image: './image/Screenshot.png',
     title: 'Multi-Post Stories Gain+Glory',
     items: ['Javascript', 'Ruby on rails', 'Html', 'Css'],
+    id: '10aa',
   },
 ];
 
-function createRecentWorkElement(data) {
+function createRecentWorkElements(data) {
   this.image = data.image;
   this.title = data.title;
   this.items = data.items;
+  this.id = data.id;
   this.createElement = function createElement() {
     const generalLiElements = (textContents) =>
       textContents
         .map((textContent) => `<li class="content-tags contrast">${textContent}</li>`)
         .join('');
     const languageLiElements = generalLiElements(this.items);
-    const listOfRecentWorkElement = document.createElement('li');
-    listOfRecentWorkElement.classList.add('grid-item');
-    listOfRecentWorkElement.innerHTML = `<div class="card">
+    const listOfRecentWorksElements = document.createElement('li');
+    listOfRecentWorksElements.classList.add('grid-item');
+    listOfRecentWorksElements.innerHTML = `<div class="card">
         <img class="card-img" src="${this.image}" alt="page" />
         <h3 class="card-header">${this.title}</h3>
         <ul class="card-content margin-left-right-auto">${languageLiElements}</ul>
         <div class="content-btn">
           <a href="" aria-label="button">
-            <button-style class="see-projects">See Project </button-style>
+            <button-style class="see-projects" ${this.id}>See Project </button-style>
           </a>
         </div>
-      </div>`;
-    return listOfRecentWorkElement;
+        </div>`;
+    return listOfRecentWorksElements;
   };
+  console.log(`${this.id}`);
 }
 
 const recentWorkWrapperElement = document.querySelector('.works');
 function attachRecentWorkDom(detail) {
-  const portfolioCard = new createRecentWorkElement(detail);
+  const portfolioCard = new createRecentWorkElements(detail);
   const portfolio = portfolioCard.createElement();
   recentWorkWrapperElement.appendChild(portfolio);
-}
-
+};
 recentWorkCards.forEach(attachRecentWorkDom);
+
 
 //navabar
 const menu = document.querySelector('.navbar-container');
